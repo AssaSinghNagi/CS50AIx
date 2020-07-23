7e57deb23e4f37b830dd5f34dfa4914600ed4d8a
Aryan Raina <66298592+Aryan-Raina@users.noreply.github.com>
2020-07-23 12:56:12 +0530

    Add files via upload

---
+++ degrees/util.py
@@ -0,0 +1,38 @@
+class Node():
+    def __init__(self, person_id, parent, movie_id):
+        self.person_id = person_id
+        self.parent = parent
+        self.movie_id = movie_id
+
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_person_id(self, person_id):
+        return any(node.person_id == person_id for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
