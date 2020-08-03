50e90a33bd2ded5b4c2449fddfb11802deea7e5e
Aryan Raina <66298592+Aryan-Raina@users.noreply.github.com>
2020-08-03 19:46:00 +0530

    Add files via upload

---
+++ heredity/test.py
@@ -0,0 +1,9 @@
+from heredity import *
+
+people = {
+  'Harry': {'name': 'Harry', 'mother': 'Lily', 'father': 'James', 'trait': None},
+  'James': {'name': 'James', 'mother': None, 'father': None, 'trait': True},
+  'Lily': {'name': 'Lily', 'mother': None, 'father': None, 'trait': False}
+}
+
+print(joint_probability(people, {"Harry"}, {"James"}, {"James"}))
